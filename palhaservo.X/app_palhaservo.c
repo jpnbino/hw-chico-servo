ffe548607fa6de5a86d73851e2b97742444ff070
jpnbino <jpnogg@gmail.com>
2018-09-26 21:17:11 -0300

    Máquina de estado finita implementada

--- palhaservo.X/app_palhaservo.c
+++ palhaservo.X/app_palhaservo.c
@@ -40,6 +40,16 @@ enum{
     CONNECTED = 1,
 };
 
+//Finite State Machine
+// ST stands for state
+typedef enum{
+    ST_INIT,
+    ST_BATTERY_TASK,
+    ST_DEAD_TIME,
+    ST_KNOB_TASK,
+    ST_SERVO_TASK,
+    ST_SERIAL_TASK,
+} state_t;
 
 /**
   Section: Included Files
@@ -66,72 +76,83 @@ void App_Palhaservo_Init ( void )
 {
     volatile uint8_t aux_device_role;
     
+    //a PULL-UP or PULL-DOWN resistor in Hardware
     aux_device_role = IO_RC1_GetValue();
 	
     Device_Set_Mode( aux_device_role );
-    
-    /**
-     * Inicia o módulo bluetooth
-     */
-    if ( Device_Get_Mode() == MASTER)
-    {   
-        volatile uint8_t aux_device_connected = 0;
-        
-        while ( aux_device_connected == NOT_CONNECTED )
-        {
-            __delay_ms(1000);
-            printf("AT+DEFAULT\r\n");
-
-            __delay_ms(1000);
-            printf("AT+POWE3\r\n");
-            
-            __delay_ms(1000);
-            printf("AT+ROLE1\r\n");
-
-            __delay_ms(1000);
-            printf("AT+INQ\r\n");
-
-            __delay_ms(5000);
-            printf("AT+CONN1\r\n");
-            __delay_ms(500);
-            aux_device_connected = IO_RC3_GetValue();
-        }
-    }
-    else if( Device_Get_Mode() == SLAVE )
-    {
-        __delay_ms(1000);
-        printf("AT+DEFAULT\r\n");
-        
-        __delay_ms(1000);
-        printf("AT+POWE3\r\n");
-
-        __delay_ms(1000);
-        printf("AT+ROLE0\r\n"); 
-    }
 }
 
 void App_Palhaservo_Run( void )
-{
-    if ( Device_Get_Mode() == MASTER )
+{  
+    static uint8_t current_state = ST_INIT;
+    static uint8_t dead_loop_counter = 0;
+    
+    switch (current_state)
     {
-        uint8_t i;
-        if(i >= 16)
-        {
-            Task_Knob_Manager();
-            i = 0;
-        }
-        else
-        {
-            ++i;
-        }
-    }
-    else if ( Device_Get_Mode() == SLAVE )
-    {
-        Task_Servo_Manager();
-    }
-    Task_Serial_Rx();
-    Task_Battery_Manager();       
-}
+        case ST_INIT:
+            
+                App_Palhaservo_Init();
+                current_state =  ST_BATTERY_TASK ;
+                
+            break;
+                        
+        case ST_BATTERY_TASK:
+
+            Task_Battery_Manager(); 
+
+            if ( Device_Get_Mode() == MASTER )
+            {
+                current_state = ST_DEAD_TIME;
+            }
+            else if ( Device_Get_Mode() == SLAVE )
+            {
+                current_state = ST_SERVO_TASK;  
+            }
+            
+            if ( IO_RC3_GetValue() == NOT_CONNECTED )
+            {
+                current_state =  ST_BATTERY_TASK ;
+            }
+
+            break;
+
+        case ST_DEAD_TIME:
+            
+            if(dead_loop_counter >= 16)
+            {
+                current_state = ST_KNOB_TASK;
+                dead_loop_counter = 0;
+            }
+            else
+            {
+                ++dead_loop_counter;
+            }
+        break;
+                
+        case ST_KNOB_TASK:
+            
+            Task_Knob_Manager();            
+
+            current_state = ST_SERIAL_TASK;
+
+            break;
+
+        case ST_SERVO_TASK:
+
+            Task_Servo_Manager();
+            current_state = ST_SERIAL_TASK;
+
+            break;
+
+        case ST_SERIAL_TASK:
+
+            Task_Serial_Rx();
+            current_state = ST_BATTERY_TASK;
+
+            break;
+    } 
+}    
+
 
 void Device_Set_Mode (uint8_t mode)
 {
